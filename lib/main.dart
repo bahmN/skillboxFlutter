674573426e254ccf1cb85e9db200a102ab8e4a6f
bahmN <iwanaev23@gmail.com>
2023-01-15 22:16:55 +0300

    module 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:material_basics/module_8/home.dart';
+import 'package:material_basics/module_9/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-/*
-    Routes
-*/
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: Colors.deepPurple,
-//       ),
-//       onGenerateRoute: ((settings) {
-//         switch (settings.name) {
-//           case MyHomePage.routeName:
-//             return PageRouteBuilder(
-//               pageBuilder: (context, animation, secondaryAnimation) =>
-//                   const MyHomePage(),
-//               transitionsBuilder:
-//                   (context, animation, secondaryAnimation, child) {
-//                 const begin = Offset(1.0, 0.0);
-//                 const end = Offset.zero;
-//                 const curve = Curves.decelerate;
-
-//                 var tween = Tween(begin: begin, end: end).chain(CurveTween(
-//                   curve: curve,
-//                 ));
-
-//                 return SlideTransition(
-//                   position: animation.drive(tween),
-//                   child: child,
-//                 );
-//               },
-//             );
-//           case AlbumsPage.routeName:
-//             return PageRouteBuilder(
-//               pageBuilder: (context, animation, secondaryAnimation) =>
-//                   const AlbumsPage(),
-//               transitionsBuilder:
-//                   (context, animation, secondaryAnimation, child) {
-//                 const begin = Offset(1.0, 0.0);
-//                 const end = Offset.zero;
-//                 const curve = Curves.decelerate;
-
-//                 var tween = Tween(begin: begin, end: end).chain(CurveTween(
-//                   curve: curve,
-//                 ));
-
-//                 return SlideTransition(
-//                   position: animation.drive(tween),
-//                   child: child,
-//                 );
-//               },
-//             );
-//           case ArtistPage.routeName:
-//             final args = settings.arguments as Map<String, dynamic>;
-//             return PageRouteBuilder(
-//               pageBuilder: (context, animation, secondaryAnimation) =>
-//                   ArtistPage(
-//                 name: args['name'],
-//                 about: args['about'],
-//               ),
-//               transitionsBuilder:
-//                   (context, animation, secondaryAnimation, child) {
-//                 const begin = Offset(1.0, 0.5);
-//                 const end = Offset.zero;
-//                 const curve = Curves.decelerate;
-
-//                 var tween = Tween(begin: begin, end: end).chain(CurveTween(
-//                   curve: curve,
-//                 ));
-
-//                 return SlideTransition(
-//                   position: animation.drive(tween),
-//                   child: child,
-//                 );
-//               },
-//             );
-//           default:
-//             print('');
-//         }
-//       }),
-//       home: const MyHomePage(),
-//     );
-//   }
-// }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color.fromARGB(255, 39, 35, 35),
+        ),
       ),
-      home: const MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => const HomeView(),
+      },
     );
   }
 }
---
+++ lib/module_9/home.dart
@@ -0,0 +1,116 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:material_basics/module_9/models/hotel.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  bool isLoading = false;
+  List<HotelPreview>? hotels;
+  Dio _dio = Dio();
+
+  @override
+  void initState() {
+    super.initState();
+    getDataDio();
+  }
+
+  getDataDio() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      final response = await _dio
+          .get('https://run.mocky.io/v3/ac888dc5-d193-4700-b12c-abb43e289301');
+      var data = response.data;
+      hotels = data
+          .map<HotelPreview>((hotel) => HotelPreview.fromJson(hotel))
+          .toList();
+      setState(() {
+        isLoading = false;
+      });
+    } on DioError catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Лучшие отели мира'),
+        actions: [
+          IconButton(
+            onPressed: () => null,
+            icon: Icon(Icons.list_sharp),
+            color: Colors.white,
+          ),
+          IconButton(
+            onPressed: () => null,
+            icon: Icon(Icons.apps_sharp),
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                ...?hotels
+                    ?.map(
+                      (data) => Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                'assets/images/hotels/${data.poster}',
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(data.name),
+                                  TextButton(
+                                    onPressed: () => null,
+                                    child: const Text('Подробнее'),
+                                    style: TextButton.styleFrom(
+                                      backgroundColor:
+                                          const Color.fromARGB(255, 39, 35, 35),
+                                      foregroundColor: Colors.white,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList()
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
