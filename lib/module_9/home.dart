3e02fdd8a039d8fadab6fd07ff8bc1b18c940c64
bahmN <iwanaev23@gmail.com>
2023-01-18 23:16:17 +0300

    module 9 ready

--- lib/module_9/home.dart
+++ lib/module_9/home.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:material_basics/module_9/details.dart';
 import 'package:material_basics/module_9/models/hotel.dart';
 
 class HomeView extends StatefulWidget {
@@ -13,8 +12,9 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   bool isLoading = false;
   List<HotelPreview>? hotels;
-  Dio _dio = Dio();
-  
+  final Dio _dio = Dio();
+  bool isList = true;
+
   @override
   void initState() {
     super.initState();
@@ -47,21 +47,23 @@ class _HomeViewState extends State<HomeView> {
     });
   }
 
+  void ListVGridV() {
+    setState(() {
+      isList ? isList = false : isList = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Лучшие отели мира'),
         actions: [
-          //TODO: Сделать "Список" и "Плитку"
           IconButton(
-            onPressed: () => null,
-            icon: const Icon(Icons.list_sharp),
-            color: Colors.white,
-          ),
-          IconButton(
-            onPressed: () => null,
-            icon: const Icon(Icons.apps_sharp),
+            onPressed: () => ListVGridV(),
+            icon: isList
+                ? const Icon(Icons.apps_sharp)
+                : const Icon(Icons.list_sharp),
             color: Colors.white,
           ),
         ],
@@ -73,34 +75,98 @@ class _HomeViewState extends State<HomeView> {
           : Column(
               children: [
                 Expanded(
-                  child: ListView(
-                    children: [
-                      ...?hotels
-                          ?.map(
-                            (data) => Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              color: const Color.fromARGB(255, 246, 245, 255),
-                              child: Column(
-                                children: [
-                                  AspectRatio(
-                                    aspectRatio: 16 / 9,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(20),
-                                      child: Image.asset(
-                                          'assets/images/hotels/${data.poster}',
-                                          fit: BoxFit.cover),
+                  child: isList
+                      ? ListView(
+                          children: [
+                            ...?hotels
+                                ?.map(
+                                  (data) => Card(
+                                    elevation: 5.5,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    color: const Color.fromARGB(
+                                        255, 246, 245, 255),
+                                    child: Column(
+                                      children: [
+                                        AspectRatio(
+                                          aspectRatio: 16 / 9,
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            child: Image.asset(
+                                                'assets/images/hotels/${data.poster}',
+                                                fit: BoxFit.cover),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 20.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(data.name),
+                                              TextButton(
+                                                onPressed: () =>
+                                                    Navigator.of(context)
+                                                        .pushNamed('/details',
+                                                            arguments: {
+                                                      'uuid': data.uuid,
+                                                    }),
+                                                style: TextButton.styleFrom(
+                                                  backgroundColor:
+                                                      const Color.fromARGB(
+                                                          255, 39, 35, 35),
+                                                  foregroundColor: Colors.white,
+                                                ),
+                                                child: const Text('Подробнее'),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 20.0),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
+                                )
+                                .toList()
+                          ],
+                        )
+                      : GridView.count(
+                          crossAxisCount: 2,
+                          children: [
+                            ...?hotels
+                                ?.map(
+                                  (data) => Card(
+                                    elevation: 5.5,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    color: const Color.fromARGB(
+                                        255, 246, 245, 255),
+                                    child: Column(
                                       children: [
-                                        Text(data.name),
+                                        AspectRatio(
+                                          aspectRatio: 16 / 10,
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            child: Image.asset(
+                                                'assets/images/hotels/${data.poster}',
+                                                fit: BoxFit.cover),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Padding(
+                                            padding:
+                                                const EdgeInsets.only(top: 5.0),
+                                            child: Text(
+                                              data.name,
+                                              textAlign: TextAlign.center,
+                                              softWrap: true,
+                                            ),
+                                          ),
+                                        ),
                                         TextButton(
                                           onPressed: () => Navigator.of(context)
                                               .pushNamed('/details',
@@ -113,18 +179,17 @@ class _HomeViewState extends State<HomeView> {
                                                     255, 39, 35, 35),
                                             foregroundColor: Colors.white,
                                           ),
-                                          child: const Text('Подробнее'),
-                                        )
+                                          child: const Text(
+                                            'Подробнее',
+                                          ),
+                                        ),
                                       ],
                                     ),
                                   ),
-                                ],
-                              ),
-                            ),
-                          )
-                          .toList()
-                    ],
-                  ),
+                                )
+                                .toList()
+                          ],
+                        ),
                 ),
               ],
             ),
